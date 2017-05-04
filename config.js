d69c28fb1800dcee96c3037a5d4a4bac3d2cd30d
HaminKo <hko1@babson.edu>
2017-05-03 22:25:43 -0700

    [Added] Config

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	DATABASE_URL: "postgres://vvsvbpnynhddtz:b0f290ecc6ec98b81607adff9b196f789564c51447627df1331bc98f9b8cdec2@ec2-23-21-220-167.compute-1.amazonaws.com:5432/d4gpfahvgooar1",
+};
