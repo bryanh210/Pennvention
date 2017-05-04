143176f26365b90e2998409dd16c76b162a8c900
HaminKo <hko1@babson.edu>
2017-05-04 02:56:06 -0700

    [Update] Team Helper Functions/Routes

--- public/scripts/studentLayoutScript.js
+++ public/scripts/studentLayoutScript.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
   $(".select2_multiple").select2({
     allowClear: true
   });
+
+  $('#tags_team_strength_1').tagsInput({
+    height:'37px',
+    width: 'auto',
+    defaultText:'Strength',
+  });
+
+  $('#tags_team_weakness_1').tagsInput({
+    height:'37px',
+    width: 'auto',
+    defaultText:'Weakness',
+  });
 });
